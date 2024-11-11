eee284654072cc9496c41e9e87fdb9b5a99d1861
Owen-Wangg <owenwang26@gmail.com>
2024-11-11 12:41:11 -0800

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import org.example.view.FitnessGoalView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
 
+        FitnessGoalView view = new FitnessGoalView();
     }
 }
