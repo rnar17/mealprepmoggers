f49e4528658c7ad0d85d793bba064d506d60b9a3
Re-Emzee <molave.liggayu@gmail.com>
2024-11-11 17:05:23 -0800

    profile.json file for user global configs ProfileController WIP TODO: get json reader dependency to work

--- src/main/java/org/example/controller/ProfileController.java
+++ src/main/java/org/example/controller/ProfileController.java
@@ -1,4 +1,26 @@
 package org.example.controller;
 
+import org.example.model.UserModel.Profile;
+
 public class ProfileController {
+    //TODO update profile.JSON through profileView
+
+    String userName;
+    int age;
+    int weight;
+    int height;
+
+    Profile user = new Profile(userName,age,weight,height);
+
+    public boolean updateProfile(){
+        //TODO read json and update fields
+        user.name = userName;
+        user.age = age;
+        user. weight = weight;
+        user.height = height;
+
+        //add condition for successful update
+        return true;
+    }
+
 }
