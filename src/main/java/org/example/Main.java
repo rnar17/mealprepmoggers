f5faf76b4c73916635417baba05b5c75f8cc0be8
zichixu <zichixu@outlook.com>
2024-11-23 15:44:10 -0800

    hehe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,6 @@
 package org.example;
 import org.example.view.MasterView;
 
-import org.example.model.SpoonacularClient;
-
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         MasterView test = new MasterView();
