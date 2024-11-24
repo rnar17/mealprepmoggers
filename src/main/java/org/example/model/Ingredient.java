02d86c34f33193d22532bec8ac049fc7841062bd
molav <molave.liggayu@gmail.com>
2024-11-24 00:30:36 -0800

    Major change, App compiles and opens: Implemented basic controller to make calls between the API and views so that the views dont directly need to interface and provide implementations for API calls.
    Refactored Client Class to provide record classes for API calls.
    
    Controller should update internal data of saved recipies, need to test how this is reflected on the view instances (May need to repaint Jpanels for each update?)

--- src/main/java/org/example/model/Ingredient.java
+++ src/main/java/org/example/model/Ingredient.java
@@ -1,5 +1,9 @@
-package org.example.model;
+package org.example.model.UserModel;
 
-public class Ingredient {
-
+public record Ingredient() {
+    public static int id;
+    public static String name;
+    public static String original;
+    public static double amount;
+    public static String unit;
 }
