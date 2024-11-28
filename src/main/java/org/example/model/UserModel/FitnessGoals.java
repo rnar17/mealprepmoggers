29388ed2937219c6270495e03ea6469b23cb6e10
Ronit Narayanan <narayanan.ronit@gmail.com>
2024-11-27 21:40:24 -0800

    changed enum

--- src/main/java/org/example/model/UserModel/FitnessGoals.java
+++ src/main/java/org/example/model/UserModel/FitnessGoals.java
@@ -4,7 +4,7 @@ public enum FitnessGoals {
     WEIGHT_LOSS("Weight Loss","• Focus on lower-calorie meals\n• Higher protein content\n• More vegetables and fiber"),
     MUSCLE_GAIN("Muscle Gain","• Higher protein meals\n• Complex carbohydrates\n• Nutrient-dense ingredients"),
     MAINTENANCE("Maintenance", "• Balanced macronutrients\n• Sustainable portion sizes\n• Variety of nutrients"),
-    NONE("No Goal","");
+    NONE("you","");
 
     public final String title;
     public final String description;
