99fb78cc622d1f665cb0a830626e73f3441d561e
Re-Emzee <molave.liggayu@gmail.com>
2024-11-29 10:55:41 -0800

    Added remove star recipe test

--- src/test/java/org/example/UnitTests/MealTests.java
+++ src/test/java/org/example/UnitTests/MealTests.java
@@ -55,14 +55,15 @@ public class MealTests {
         assertTrue(recipes.containsAll(mealController.getFavouriteRecipes()));
     }
 
-//    @Test
-//    public void removeStar() throws Exception {
-//        MealController mealController = new MealController();
-//        List<String> ingredients = new ArrayList<>();
-//        ingredients.add("egg");
-//        List<Recipe> recipes = mealController.findRecipies(ingredients);
-//        mealController.addRecipies(recipes);
-//        recipes.forEach(mealController::starRecipe);
-//        assertTrue(recipes.containsAll(mealController.getFavouriteRecipes()));
-//    }
+    @Test
+    public void removeStar() throws Exception {
+        MealController mealController = new MealController();
+        List<String> ingredients = new ArrayList<>();
+        ingredients.add("egg");
+        List<Recipe> recipes = mealController.findRecipies(ingredients);
+        mealController.addRecipies(recipes);
+        recipes.forEach(mealController::starRecipe);
+        mealController.removeStarRecipe(recipes.get(0));
+        assertEquals(5,mealController.getFavouriteRecipes().size());
+    }
 }
